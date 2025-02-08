0c2af3b670dbc9b7ada95cb368d375b2edd564fc
tophrchrs <justchrstopherachilles@gmail.com>
2025-02-08 22:49:24 +0700

    ninteenth

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+    })],
+    server: {
+        hmr: {
+            host: 'laravel-app-deploy-64zki8pnk-christophers-projects-92ace75d.vercel.app',
+            protocol: 'wss',
+        },
+    },
 });
